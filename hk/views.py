a8dd94b2b336af25c7ac9082d108b4b8e81118d8
seday72 <seday72@yandex.com>
2020-10-15 08:06:07 +0300

    livestream_delete

--- hk/views.py
+++ hk/views.py
@@ -189,53 +189,7 @@ def start_sync_products():
     mapi = mongo_connect(mongo['url'])
     mongo_db = mapi[mongo['dbname']]
     m_products = mongo_db['products'].find()
-    fixed = ["9b376793-36ae-45b6-9831-02a7362fe92f", "5b672ab0-7fb6-4a04-8fb2-b8eee17e9b43", "28149e68-e092-4572-9cdf-fdd295c2a6a0", "fa845ec8-bef4-4964-bfee-1b05ee47e0b3",
-             "c5c9cbc1-a121-4069-960a-dbd40b34d249", "449d0012-14f6-40a1-a2e6-30ef6f3611c7", "2ddcf011-142d-43a3-b6e9-99ca30cb6b11", "1fe1af0f-be47-4dd0-8cc1-73b2c35e5235",
-             "b7371328-c708-4580-8024-97504461e92a", "c76b7cf6-b505-4d95-8e12-ee3592bb1ed0", "721b45b8-9f1a-4a96-9e14-117f1b01cb69", "4fda4d1e-92d7-4a62-9387-1c59a8b7d589",
-             "832e6d3d-5aae-459f-865c-776cbc68bfeb", "78fe5543-e268-4a6c-8ea1-8f0aa834ef4b", "3440a1e5-4e54-45c6-8544-cf7e16697358", "28149e68-e092-4572-9cdf-fdd295c2a6a0",
-             "f65b52f3-f15f-44e2-9f56-09c14cfe3042", "f01cc95e-0f58-43b6-a7b5-544b9fed14ce", "f20f498c-2096-4516-867d-c51f59b192d8", "50ab156c-e1a8-41a6-b9e6-5bef1f09aaf8",
-             "d584aaf1-0fe0-45b4-bb9a-6bf3f5167aa8", "f01cc95e-0f58-43b6-a7b5-544b9fed14ce", "f20f498c-2096-4516-867d-c51f59b192d8", "50ab156c-e1a8-41a6-b9e6-5bef1f09aaf8",
-             "af26e2f7-d0c1-466f-91f5-f798244b37cb", "6f67a93b-2d11-46a7-bfd6-832f5db6a3f3", "8fbfc41a-cccf-4c8b-b3a2-a3196267ee80", "7b8a995b-cd86-4578-a397-fea55ac5fa20",
-             "a5abffaf-3dfa-49d4-a392-b0e80712fb80", "c88d74af-d040-4151-aff2-d7b64db429df", "e2940234-db43-4c70-b2ac-83109b8f7f6c", "45c4dc71-3c05-428e-acb7-55cc541c72de",
-             "91b10d7a-84c9-47e3-a834-86a4d7c2e991", "fa59df64-70b4-4a3c-9fca-748d728adf5c", "846ac6dc-67ed-4e36-b68f-2b5a24cee528", "b88ed9bd-816d-48ed-ba84-c1c9cc92224d",
-             "f60dc0cd-43ba-4370-b5d8-4ace3f78a4b3", "d9d731e0-68bf-4b2e-a231-5a6271f7e2f4", "d4849ff3-00cd-40f1-bdfc-ff3b3eb62b61", "ac53ebb6-96c1-4318-83b5-b5c822a978de",
-             "238afa3c-d184-4ecc-bc2c-dce6b60634d1", "ee08d8b5-2d72-4eab-8781-171bc442f1c1", "08235b21-222b-4b1d-ab9f-f0798dfa0fa6", "a1954caf-a238-4623-b833-e01e6b6e4259",
-             "b271b7f5-2442-4fe2-9c40-d55444dfc467", "0e2f3844-16ec-4092-812a-3d05c0fd458c", "9648614e-2d37-46c1-beb0-5b40dabc5d01", "0fe34f91-1bcd-4193-9c09-082be4520c82",
-             "157181bf-2aec-4a76-b66d-5c809400b737", "abc78d70-f6f7-467f-a156-60db5eabcd99", "20727d76-f966-4d36-a811-b8568d163bf4", "821c0dd2-1c47-480e-8151-d12a77cb7c4b",
-             "7d0e02cd-9812-41b8-adb4-cf83fed57214", "6f219fd1-24f9-4f1a-8841-78e59976f6b2", "1296459f-f3e3-4144-8d97-82c6f53da38f", "c57dd046-ceff-46a1-bac2-583d763c2025",
-             "0f2fcd34-e41a-438e-b2a3-5fdd797f87f3", "9648614e-2d37-46c1-beb0-5b40dabc5d01", "0fe34f91-1bcd-4193-9c09-082be4520c82", "157181bf-2aec-4a76-b66d-5c809400b737",
-             "abc78d70-f6f7-467f-a156-60db5eabcd99", "20727d76-f966-4d36-a811-b8568d163bf4", "821c0dd2-1c47-480e-8151-d12a77cb7c4b", "7d0e02cd-9812-41b8-adb4-cf83fed57214",
-             "6f219fd1-24f9-4f1a-8841-78e59976f6b2", "1296459f-f3e3-4144-8d97-82c6f53da38f", "c57dd046-ceff-46a1-bac2-583d763c2025", "0f2fcd34-e41a-438e-b2a3-5fdd797f87f3",
-             "d9c38e28-2998-4078-88fe-6cd51931dcc0", "da55bf8c-a351-4d9b-b084-02f5f7928c21", "55f894e4-b84e-4fb2-9ed3-915eefcd7f7b", "838d78e9-e91e-4051-9e6a-6d3eb7362212",
-             "3849e792-a813-494e-b5c5-507abd80121d", "3d7c72eb-d691-4beb-9fe4-9abb3522e919", "93502ac5-a2a3-4f10-9c3a-16127c81e716", "14f26a1a-7678-432f-ac87-b6d8e0c62b27",
-             "fd2c453f-6a2b-41f0-9f7c-bda29b660a51", "f67d4852-54f4-4394-aefc-52cb60438830", "fd596191-41c3-4562-ac20-ad04c8a55d4c", "d7d2f4e1-757c-4f97-8db7-c08744e26c17",
-             "f333286f-638a-44db-a2ee-cd5f89442562", "d00a9866-5d25-4966-a4bb-e88eb149f236", "08f441c0-2455-4cd3-becf-4fd285493651", "c79521b5-f12e-4890-87f3-cc8b674a6097",
-             "0b0a85bc-95e1-488b-987d-ca36bcafc807", "55fe6c14-7bc0-4921-b4dc-6ddef961cecc", "8de909b0-d6a3-4651-bb6f-749717c75a5f", "c08912a7-80c1-4223-be94-5f00c1d3dc05",
-             "b532b29f-f8fd-4058-af14-0e807b358fe5", "e21180cf-3f01-4a6c-be29-d22b182b36f0", "2017aeda-f34d-4775-89f4-86235de3afb3", "b409a371-103e-468f-8c38-8b7151f30a6d",
-             "fe31edba-2623-41db-80a6-35bb0af97f7f", "2017aeda-f34d-4775-89f4-86235de3afb3", "b409a371-103e-468f-8c38-8b7151f30a6d", "fe31edba-2623-41db-80a6-35bb0af97f7f",
-             "dab12f34-9c7c-42b6-a5fe-7b963feb680b", "ed03f28f-c432-4b3f-8a18-e741556669fd", "860059f4-4de1-4ae1-b024-7ea8eb6b21bf", "dab12f34-9c7c-42b6-a5fe-7b963feb680b",
-             "ed03f28f-c432-4b3f-8a18-e741556669fd", "860059f4-4de1-4ae1-b024-7ea8eb6b21bf", "5ea1cf85-6c17-445c-bf91-274e91a335de", "b6305f0b-5cfb-4287-b3d8-02bb1bbac895",
-             "c373d58f-adef-4647-ab5f-6c3a4d1fa14e", "86c72fba-53a2-4bbc-8d1f-680b9637df2b", "35fb0b1b-77df-4e80-88e0-69b943227cc6", "5408eea8-3f9a-478c-8ea2-fbaf66d5c1af",
-             "47c33bff-a94d-4684-b0b4-6aa9fc159a7e", "cb08e5fa-6fd6-47b5-95dd-ad136bf73789", "e4e7af08-d075-48a1-b502-1508d1c5da42", "6779c908-d061-44a6-8dda-e06c4bc982d8",
-             "17a6bf40-cc24-4fec-8f26-b1d2c84df318", "d9c38e28-2998-4078-88fe-6cd51931dcc0", "265959fd-2822-4907-866a-cc6d5d3adbe4", "7017a0f9-36a7-49d7-b35d-343c67f8f72a",
-             "20896a53-48fe-4e4b-b322-1f424c99cbac", "d2de599a-00e4-4611-8547-ec47d7207c46", ]
-    fix_products = [
-        "721b45b8-9f1a-4a96-9e14-117f1b01cb69", "832e6d3d-5aae-459f-865c-776cbc68bfeb", "4fda4d1e-92d7-4a62-9387-1c59a8b7d589"]
 
-    have_to_fix = [
-        "2c51457a-3384-44f9-b202-b8dbf6a5cb3e",
-        "bc8f4983-77de-4249-9d56-bab33fc70d54",
-        "c5220c3d-ce36-480a-8bd5-0e1799ddca2e",
-        "d74c0ea6-3940-4c13-a729-e68e6ce38498",
-        "8aeca80e-697c-4758-bd3d-233544f6eb4c",
-        "89fa9898-21b9-40fb-aba3-2ebdd11132e6",
-        "af26e2f7-d0c1-466f-91f5-f798244b37cb",
-        "14c3870b-42dd-41c0-88d2-6e4a4d0b90ca",
-        "1cedd638-7c3f-4060-a0be-5a116b965038",
-        "a900aa55-9f8c-4480-ae6e-a6529fe52655",
-        "84b72bef-6ba6-4e7e-a353-68dbf872601f",
-        "7d397ea5-17b0-4893-ae37-298c69c67605"
-    ]
     for mp in m_products:
         sync_statues = get_status('products')
         if sync_statues.state == 0:
