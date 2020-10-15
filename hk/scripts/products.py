887276ba7d22a9b6566ddae51a2a25a249842bfb
seday72 <seday72@yandex.com>
2020-10-15 08:01:54 +0300

    livestream_delete

--- hk/scripts/products.py
+++ hk/scripts/products.py
@@ -166,36 +166,6 @@ def update_product(mapi, wapi, woo_id, mongo_product, cc_rate):
     if int(woo_tag_id) > 0:
         product_data['tags'] = [{'id': woo_tag_id}]
 
-    mp_assets = []
-    if mongo_product['assets']:
-        for mp_image in mongo_product['assets']:
-            p_image_from_log = get_image_from_log(mongo_id=mp_image, category='product')
-            if p_image_from_log:
-                mp_assets.append({'id': p_image_from_log.woo_id})
-            else:
-                mongo_asset = mapi['assets'].find_one({'_id': mp_image})
-                # check if image exist on url
-                if mongo_asset:
-                    mp_assets.append({
-                        'src':  mongo_asset['url'],
-                        'name': mongo_asset['_id']
-                    })
-                    # response = requests.head(mongo_asset['url'])
-                    # image_formats = ("image/png", "image/jpeg", "image/jpg")
-                    # if response.headers['content-type'] in image_formats:
-                    #     mp_assets.append({
-                    #         'src':  mongo_asset['url'],
-                    #         'name': mongo_asset['_id']
-                    #     })
-                    # else:
-                    #     print('invalid image', response.headers['content-type'])
-                    #     invalid_asset_to_log = InvalidAssets(mongo_id=mongo_asset['_id'])
-                    #     invalid_asset_to_log.save()
-                else:
-                    invalid_asset_to_log = InvalidAssets(mongo_id=mongo_asset['_id'])
-                    invalid_asset_to_log.save()
-    if mp_assets:
-        product_data['images'] = mp_assets
     woo_ins_data = woo_product_update(wapi, woo_id, product_data)
 
     exist_author = get_user_from_log(mongo_id=mongo_product['seller'])
