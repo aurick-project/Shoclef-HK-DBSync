2d0db8422d5fc443551d6283e9f2a4b6ad149684
seday72 <seday72@yandex.com>
2020-10-17 18:33:52 +0300

    livestream_delete

--- hk/scripts/graphql_connect.py
+++ hk/scripts/graphql_connect.py
@@ -2,18 +2,17 @@ from python_graphql_client import GraphqlClient
 import asyncio
 
 # Instantiate the client with an endpoint.
-client = GraphqlClient(endpoint="https://countries.trevorblades.com")
+client = GraphqlClient(endpoint="http://52.59.243.101:4000/graphql")
 
 # Create the query string and variables required for the request.
 query = """
-    query countryQuery($countryCode: String) {
-        country(code:$countryCode) {
-            code
-            name
+    query GetProduct{
+        product(id:"fff00a1e-f7b1-40ac-ac62-1fe2924bd149"){
+            title
+            description
         }
     }
 """
-variables = {"countryCode": "CA"}
 
 # # Synchronous request
 # data = client.execute(query=query, variables=variables)
@@ -22,5 +21,5 @@ variables = {"countryCode": "CA"}
 
 # Asynchronous request
 
-data = asyncio.run(client.execute_async(query=query, variables=variables))
+data = asyncio.run(client.execute_async(query=query))
 print(data)  # => {'data': {'country': {'code': 'CA', 'name': 'Canada'}}}
