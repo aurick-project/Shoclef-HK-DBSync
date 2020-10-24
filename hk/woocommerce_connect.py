5e1208129dcae4da034ec55e98afcd07529c0d1a
seday72 <seday72@yandex.com>
2020-10-25 00:41:05 +0900

    add shipping class - init

--- hk/woocommerce_connect.py
+++ hk/woocommerce_connect.py
@@ -24,7 +24,7 @@ def woo_products(wcapi, page=1, per_page=100):
     try:
         print('get product page: %s per_page: %s' % (page, per_page))
         products = wcapi.get('products', params={'page': page, 'per_page': per_page})
-        print(products.content)
+        print(products.text)
         if products:
             return products.json()
     except Exception as e:
