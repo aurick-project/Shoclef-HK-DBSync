2d06e60761a46f73cdc567a56d3c746bbaef1466
seday72 <seday72@yandex.com>
2020-10-29 04:53:52 +0900

    product csv

--- hk/views.py
+++ hk/views.py
@@ -521,7 +521,7 @@ def start_sync_products():
             writer.writeheader()
             for csv_value in products_no_variation:
                 writer.writerow(csv_value)
-        print([d['id'] for d in products_no_variation])
+        # print([d['id'] for d in products_no_variation])
     save_status('products', 0)
 
 
