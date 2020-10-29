d955ae92959c503badeca63cbdf18c9a81a47ddc
seday72 <seday72@yandex.com>
2020-10-29 15:22:12 +0900

    update product author

--- hk/views.py
+++ hk/views.py
@@ -286,6 +286,11 @@ def start_sync_products():
     with open('uploads/products-shoclef.com-modified-10282020.csv', 'r', encoding='utf-8') as csvfile:
         csv_values = csv.DictReader(csvfile)
         for csv_value in csv_values:
+            sync_statues = get_status('products')
+            if sync_statues.state == 0:
+                print('-' * 30)
+                print('Break syncing...')
+                break
             # print(csv_value['title'], csv_value['email'])
             post_from_mysql = mysql_select_table(mysql_cursor, 'wp_posts', where='post_title="%s"' % csv_value['title'], fetch='one')
             if post_from_mysql:
